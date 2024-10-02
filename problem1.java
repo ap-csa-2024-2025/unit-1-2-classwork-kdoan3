3150c9dff0f2bbb8c88bdfa6cd05f0ed8d60c8e4
kdoan3 <kbdoan103@student.hbuhsd.edu>
2024-10-02 10:54:50 -0700

    fdsa

--- problem1.java
+++ problem1.java
@@ -1,1 +1,8 @@
-// Create your class here; don't forget the main method
+public class problem1
+{
+    public static void main(String[] args)
+    {
+        String favfood = "poke";
+        System.out.print("I like to eat " + favfood " as well!");
+    }
+}
